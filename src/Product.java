3b2cb8580269dc3e0c9865838aecb00428be6939
smithca <sunsmith126@gmail.com>
2023-11-01 11:42:21 -0500

    a message

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,2 @@
-public class Product {
+public abstract class Product {
 }
---
+++ src/example/Example.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Example {
+
+    //Code using factory pattern
+//    public interface IThingFactory
+//    {
+//        Thing GetThing(string theString);
+//    }
+//
+//    public class ThingFactory : IThingFactory
+//    {
+//        public Thing GetThing(string theString)
+//        {
+//            return new Thing(theString, firstDependency, secondDependency);
+//        }
+//    }
+
+}
