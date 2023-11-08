44be6fdb01a9755c4ce52e979bbb37420b9e0f86
pearsalla <pearsalla@msoe.edu>
2023-11-08 10:44:26 -0600

    thread

--- src/threading/part1/ThreadingApp.java
+++ src/threading/part1/ThreadingApp.java
@@ -1,26 +1,39 @@
 package threading.part1;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ThreadingApp {
-    private List<Integer> theList;
+public class ThreadingApp implements Runnable {
+    private List<Integer> theList = new ArrayList<>();
 
-    public ThreadingApp() {
+    public ThreadingApp() throws InterruptedException {
+        int count = 10000;
+
         Thread thread = new Thread();
         thread.start();
-        addElements();
-
+        System.out.println(thread.isAlive());
+        run();
+        addElements(count);
+        thread.join();
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
 
         ThreadingApp threadingApp = new ThreadingApp();
-
-
-
     }
 
-    public void addElements() {
+    public void addElements(int count) {
+        //System.out.println("Working on thread");
 
+        for (int i = 0; i < count; i++) {
+            int num = (int) Math.round((Math.random() * 10000));
+            theList.add(num);
+            System.out.println(theList.toString());
+        }
     }
 
+    @Override
+    public void run() {
+        System.out.println("Thread is running");
+    }
 }
